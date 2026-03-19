56275190f6a5da012303c3867ac74cc748c104af
Beuxj <ritvikp2023@gmail.com>
2026-03-19 23:14:33 +0530

    added techinical skills bar , changed the animatin style with a little glow effect and changed the layout

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 function scrollToSection(id) {
-    const section = document.getElementById(id);
-    section.scrollIntoView({ behavior: "smooth" });
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
 }
+
+/* SCROLL REVEAL */
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+    const windowHeight = window.innerHeight;
+
+    reveals.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+
+        if (top < windowHeight - 100) {
+            el.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
+
+/* SKILL ANIMATION */
+const bars = document.querySelectorAll(".progress");
+
+function animateSkills() {
+    bars.forEach(bar => {
+        const top = bar.getBoundingClientRect().top;
+
+        if (top < window.innerHeight - 50) {
+            bar.style.width = bar.dataset.width;
+        }
+    });
+}
+
+window.addEventListener("scroll", animateSkills);
+window.addEventListener("load", animateSkills);
